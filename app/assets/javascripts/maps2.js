db26f17e5113a4ec4abb41f70eb121ecefc58417
Dan Avery <dan.github@danavery.com>
2013-01-22 12:01:50 -0800

    cartoDB with infowindow

--- app/assets/javascripts/maps2.js
+++ app/assets/javascripts/maps2.js
@@ -1,27 +1,34 @@
 $(function () {
 
-  function initializeMap() {
+  // function initializeMap() {
 
-    var latlng = new google.maps.LatLng(41.073056, -81.517778);
-    var mapOptions = {
-      zoom: 10,
-      center: latlng,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+  //   var latlng = new google.maps.LatLng(41.073056, -81.517778);
+  //   var mapOptions = {
+  //     zoom: 10,
+  //     center: latlng,
+  //     mapTypeId: google.maps.MapTypeId.ROADMAP
+  //   };
+  //   var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
     
-    cartodb.createLayer(map, {
-      type: 'cartodb',
-      options: {
-       table: 'locations',
-       user_name: 'danavery',
-       query: 'select * from locations'
-      }
-    }).done(function(layer) { 
-      console.log(layer);
-      map.overlayMapTypes.setAt(0, layer);
-      addClickBehavior(layer);
-    });
+  //   cartodb.createLayer(map, {
+  //     type: 'cartodb',
+  //     options: {
+  //      table: 'locations',
+  //      user_name: 'danavery',
+  //      query: 'select * from locations'
+  //     }
+  //   }).done(function(layer) { 
+  //     console.log(layer);
+  //     map.overlayMapTypes.setAt(0, layer);
+  //     addClickBehavior(layer);
+  //   });
+  // }
+
+  function initializeMap() {
+    cartodb.createVis('map_canvas',"http://danavery.cartodb.com/api/v1/viz/17246/viz.json")
+    .done(function(vis, layers) {
+      addClickBehavior(layers[1]);
+    })
   }
 
   function addClickBehavior(layer) {
