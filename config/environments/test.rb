2da40de8d36762dbc7e38d44226d534fef7eb7d9
Dan Avery <dan.github@danavery.com>
2013-04-11 14:22:59 -0700

    Fixing typo in test.rb

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,4 @@
-getoutside::Application.configure do
+Getoutside::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # The test environment is used exclusively to run your application's
